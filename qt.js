4409e8c2cedd188baeff1108892ccebc1856ff71
Emily Adey <eadey121122@yahoo.com>
2018-12-20 23:49:44 -0500

    adding the rest of the files

---
+++ qt.js
@@ -0,0 +1,20 @@
+
+var left = $('#sun').offset().left;
+
+$("#sun").css({left:left}).animate({"left":"840px"}, "fast");
+
+
+var left = $('#ray').offset().left;
+
+$("#ray").css({left:left}).animate({"left":"820px"}, "fast");
+
+
+var left = $('#ray3').offset().left;
+
+$("#ray3").css({left:left}).animate({"left":"880px"}, "fast");
+
+var left = $('#ray2').offset().left;
+
+$("#ray2").css({left:left}).animate({"left":"860px"}, "slow");
+
+ 
